c8997ba344e85ac8b1b85a429d63aa825af9f538
Jan Smolka <onkelsmo@gmail.com>
2014-08-26 11:50:28 +0200

    initial

---
+++ SpaceRaid/SpaceRaid.Windows/Elements/Raider.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+
+namespace SpaceRaid.Elements
+{
+    class Raider
+    {
+        private int hitPoints;
+        private string position;
+        private int[] coords;
+        private int range;
+        private Grid lastTile;
+
+        public int getHp()
+        {
+            return this.hitPoints;
+        }
+        public void setHp(int value)
+        {
+            this.hitPoints = value;
+        }
+        public string getPosition()
+        {
+            return this.position;
+        }
+        public void setPosition(string value)
+        {
+            if (this.isMoveAllowed(value))
+            {
+                this.position = value;
+                this.setCoords(value);
+            }
+            // TODO: else?
+        }
+        public int getRange()
+        {
+            return this.range;
+        }
+        public int[] getCoords()
+        {
+            return this.coords;
+        }
+        private void setCoords(string value)
+        {
+            this.coords = new int[] { Convert.ToInt32(value.Substring(8, 1)), Convert.ToInt32(value.Substring(9, 1)) };
+        }
+
+        public Raider()
+        {
+            this.hitPoints = 5;
+            this.position = "tileGrid41";
+            this.setCoords(this.position);
+            this.range = 1;
+        }
+
+        public void display(Grid tileGrid)
+        {
+            this.lastTile = tileGrid;
+
+            tileGrid.Background = new SolidColorBrush(Colors.Tomato);
+        }
+        
+        private bool isMoveAllowed(string value)
+        {
+            int[] tileNumber = new int[] { Convert.ToInt32(value.Substring(8, 1)), Convert.ToInt32(value.Substring(9, 1)) };
+
+            if (tileNumber[0] == this.coords[0]+1 ||
+                tileNumber[0] == this.coords[0]-1 ||
+                tileNumber[1] == this.coords[1]+1 ||
+                tileNumber[1] == this.coords[1]-1)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ SpaceRaid/SpaceRaid.Windows/MainPage.xaml.cs
@@ -0,0 +1,149 @@
+﻿using SpaceRaid.Common;
+using SpaceRaid.Elements;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// Die Elementvorlage "Standardseite" ist unter http://go.microsoft.com/fwlink/?LinkId=234237 dokumentiert.
+
+namespace SpaceRaid
+{
+    /// <summary>
+    /// Eine Standardseite mit Eigenschaften, die die meisten Anwendungen aufweisen.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private NavigationHelper navigationHelper;
+        private ObservableDictionary defaultViewModel = new ObservableDictionary();
+        private Raider raider;
+        private int tiles;
+        private int score;
+        
+        /// <summary>
+        /// Dies kann in ein stark typisiertes Anzeigemodell geändert werden.
+        /// </summary>
+        public ObservableDictionary DefaultViewModel
+        {
+            get { return this.defaultViewModel; }
+        }
+
+        /// <summary>
+        /// NavigationHelper wird auf jeder Seite zur Unterstützung bei der Navigation verwendet und 
+        /// Verwaltung der Prozesslebensdauer
+        /// </summary>
+        public NavigationHelper NavigationHelper
+        {
+            get { return this.navigationHelper; }
+        }
+
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.navigationHelper = new NavigationHelper(this);
+            this.navigationHelper.LoadState += navigationHelper_LoadState;
+            this.navigationHelper.SaveState += navigationHelper_SaveState;
+
+            this.tiles = 0;
+            this.score = 0;
+
+            this.generatePlayField();
+        }
+
+        /// <summary>
+        /// Füllt die Seite mit Inhalt auf, der bei der Navigation übergeben wird. Gespeicherte Zustände werden ebenfalls
+        /// bereitgestellt, wenn eine Seite aus einer vorherigen Sitzung neu erstellt wird.
+        /// </summary>
+        /// <param name="sender">
+        /// Die Quelle des Ereignisses, normalerweise <see cref="NavigationHelper"/>
+        /// </param>
+        /// <param name="e">Ereignisdaten, die die Navigationsparameter bereitstellen, die an
+        /// <see cref="Frame.Navigate(Type, Object)"/> als diese Seite ursprünglich angefordert wurde und
+        /// ein Wörterbuch des Zustands, der von dieser Seite während einer früheren
+        /// beibehalten wurde. Der Zustand ist beim ersten Aufrufen einer Seite NULL.</param>
+        private void navigationHelper_LoadState(object sender, LoadStateEventArgs e)
+        {
+        }
+
+        /// <summary>
+        /// Behält den dieser Seite zugeordneten Zustand bei, wenn die Anwendung angehalten oder
+        /// die Seite im Navigationscache verworfen wird.  Die Werte müssen den Serialisierungsanforderungen
+        /// von <see cref="SuspensionManager.SessionState"/> entsprechen.
+        /// </summary>
+        /// <param name="sender">Die Quelle des Ereignisses, normalerweise <see cref="NavigationHelper"/></param>
+        /// <param name="e">Ereignisdaten, die ein leeres Wörterbuch zum Auffüllen bereitstellen
+        /// serialisierbarer Zustand.</param>
+        private void navigationHelper_SaveState(object sender, SaveStateEventArgs e)
+        {
+        }
+
+        #region NavigationHelper-Registrierung
+
+        /// Die in diesem Abschnitt bereitgestellten Methoden werden einfach verwendet, um
+        /// damit NavigationHelper auf die Navigationsmethoden der Seite reagieren kann.
+        /// 
+        /// Platzieren Sie seitenspezifische Logik in Ereignishandlern für  
+        /// <see cref="GridCS.Common.NavigationHelper.LoadState"/>
+        /// und <see cref="GridCS.Common.NavigationHelper.SaveState"/>.
+        /// Der Navigationsparameter ist in der LoadState-Methode verfügbar 
+        /// zusätzlich zum Seitenzustand, der während einer früheren Sitzung beibehalten wurde.
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            navigationHelper.OnNavigatedTo(e);
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            navigationHelper.OnNavigatedFrom(e);
+        }
+
+        #endregion
+
+        private void generatePlayField()
+        {
+            // setup the Raider
+            this.raider = new Raider();
+            this.raider.display(tileGrid41);
+
+            // setup the information Panel
+            tblRaider.Text = this.raider.ToString();
+            tblHp.Text = this.raider.getHp().ToString();
+            tblTiles.Text = tiles.ToString();
+            tblScore.Text = score.ToString();
+        }
+
+        private void tileGrid_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            Grid lastTile = playFieldGrid.FindName(this.raider.getPosition()) as Grid;
+            lastTile.Background = new SolidColorBrush(Colors.Black);
+         
+            // move raider   
+            Grid grid = sender as Grid;
+            this.raider.setPosition(grid.Name);
+            // TODO: display has to wait if move is allowed or not
+            this.raider.display(grid);
+
+            // set tiles counter
+            this.tiles++;
+            tblTiles.Text = tiles.ToString();
+
+            // log the move
+            string curentText = tbOutput.Text;
+            tbOutput.Text = "Raider moved to " + grid.Name + "\n" + curentText;
+        }
+    }
+}
