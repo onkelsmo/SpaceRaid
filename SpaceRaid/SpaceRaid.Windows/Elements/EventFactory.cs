7df4ef67dd350db678f0badf24ed18380080553b
Jan Smolka <onkelsmo@gmail.com>
2014-08-26 16:26:57 +0200

    ADD: eventFactory class

---
+++ SpaceRaid/SpaceRaid.Windows/Elements/EventFactory.cs
@@ -0,0 +1,27 @@
+﻿using SpaceRaid.Elements.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceRaid.Elements
+{
+    class EventFactory
+    {
+        private Event[] eventArray;
+
+        public EventFactory()
+        {
+            eventArray = new Event[] { new Positiv(), new Negativ(), new Empty() };
+        }
+
+        public Event getEvent()
+        {
+            Random rnd = new Random();
+            int r = rnd.Next(eventArray.Length);
+
+            return this.eventArray[r];
+        }
+    }
+}
